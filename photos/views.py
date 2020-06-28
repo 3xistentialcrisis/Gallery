b3d0fc0a50914ecb5145c56af021a52b112b0f0a
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-28 15:37:27 +0300

    Create image search results view

--- photos/views.py
+++ photos/views.py
@@ -3,8 +3,21 @@ from django.http import HttpResponse
 from .models import Image, Location
 
 # Create your views here.
+#Display Index Page
 def index(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     print(locations)
     return render(request, 'photos/index.html',{'images': images[::-1], 'locations': locations})
+
+#View Search Results
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'pictures/search_results.html', {"message": message})
